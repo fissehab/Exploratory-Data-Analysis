bb68836db5ee4340d454d119ee11cd9957fb12e3
fissehab <fishjhu@gmail.com>
2015-08-16 21:54:41 -0400

    minor edit

--- plot4.R
+++ plot4.R
@@ -46,7 +46,7 @@ plot(data2$DateTime, as.numeric(as.character(data2$Sub_metering_1)),type='l', xl
 lines(data2$DateTime, as.numeric(as.character(data2$Sub_metering_2)),type='l', col='red')
 lines(data2$DateTime, data2$Sub_metering_3,type='l', col="blue")
 legend('topright', c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
-       lty=c(1,1,1),col=c("black","red","blue"))
+       lty=c(1,1,1),col=c("black","red","blue"),bty = "n")
 
 plot(data2$DateTime, as.numeric(as.character(data2$Voltage)),type='l', 
      ylab="Voltage",xlab="datetime" )
